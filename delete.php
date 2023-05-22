c6c81c763232a8ef9f094423ff731fb86527ee61
RabbaniNabilMusyaffa <nabil.musyaffa.rn@gmail.com>
2023-05-22 18:56:25 +0700

    Optimisasi Function Delete

--- delete.php
+++ delete.php
@@ -1,22 +1,39 @@
 <?php
-//Koneksi ke database
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "datasiswa";
+include('conn.php');
 
-$conn = new mysqli($server, $username, $password, $database);
+global $conn;
+$id = $_GET['id'];
 
+global $id;
 
-if (isset($_GET['id'])) {
-    if (empty($_GET['id'])) {
-        echo "<b>Data yang dihapus tidak ada</b>";
-    } else {
-        $delete = mysqli_query($conn, "DELETE FROM siswa WHERE id='$_GET[id]'") or die("Mysql Error : " . mysqli_error($conn));
-        if ($delete) {
+$sql = "DELETE FROM siswa WHERE id = ?";
+if ($stmt = mysqli_prepare($conn, $sql)) {
+    mysqli_stmt_bind_param($stmt, "i", $param_id);
+    $param_id = $id;
+
+    $sql = "SELECT * FROM siswa WHERE id = ?";
+    if ($prepare = mysqli_prepare($conn, $sql)) {
+        mysqli_stmt_bind_param($prepare, "i", $param_id);
+        mysqli_stmt_execute($prepare);
+        $result = mysqli_stmt_get_result($prepare);
+        if (mysqli_num_rows($result) == 1) {
+            $delete = mysqli_fetch_array($result);
+            $del_foto = unlink("foto/$delete[foto]");
             echo "Berhasil Hapus Data <br>";
             echo "<a href='detail.php'>Kembali</a>";
+        } else {
+            header("location: error");
+            exit();
+        }
+        if (mysqli_stmt_execute($stmt) && ($del_foto)) {
+            return true;
+        } else {
+            return false;
         }
     }
+
+    mysqli_stmt_close($stmt);
+    mysqli_stmt_close($prepare);
 }
+
 ?>
